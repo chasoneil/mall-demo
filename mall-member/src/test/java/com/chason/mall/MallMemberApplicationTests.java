4fa80c747fb00763a3cb4f38b4ce61de2ecbb439
chason <chason001@126.com>
2023-07-21 15:10:53 +0800

    initializing project to github

---
+++ mall-member/src/test/java/com/chason/mall/MallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chason.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
